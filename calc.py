54879fa1aa254eb7bc4a432761b31939de9a7654
sabishekkrishna <ataprobe@gmail.com>
2026-09-10 23:32:33 +0530

    second commit

---
+++ calc.py
@@ -0,0 +1,10 @@
+num1=13
+num2=4
+print("Addition is ",num1+num2)
+print("Subtraction is ",num1-num2)
+print("Multiplication is ",num1*num2)
+
+if num2!=0:
+    print("Division is ",num1/num2)
+else:
+    print("Division by 0 is not possible ")
