e66fa9fb8c66da16cec00443a22f3626b24276c7
sabishekkrishna <ataprobe@gmail.com>
2026-09-10 22:53:34 +0530

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 def find_max(numbers): 
-    returnmax(numbers) 
+    return max(numbers) 
 def count_evens(numbers): 
-    returnlen([n forn innumbers ifn% 2 ==0])
+    return len([n for n in numbers if n% 2 ==0])
